17b295c3afd0fbbed8eba1c5fb2a64fa8708618b
geetgovind <geetg.2902@gmail.com>
2024-02-20 15:08:17 +0530

    Added two different page tsx files in dashoard in app and app/ui

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function dashboard() {
+    return (
+        <div>dashboard page with route app/dashboard</div>
+    )
+}
---
+++ app/ui/dashboard/page.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// export const dashboard =async () => {
+//   return (
+//     <div>dashboard page in app/ui/dashboard</div>
+//   )
+// }
+
+import React from 'react';
+
+export default function dashboard() {
+  return <div>dashboard page in app/ui/dashboard</div>;
+}
