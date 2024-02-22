640e7bf6de3532f829956f7bf8a0c831a92e0bc5
geetgovind <geetg.2902@gmail.com>
2024-02-22 14:11:55 +0530

    Completed fetching data from DB using data.ts and functions

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -1,26 +1,50 @@
-// import React from 'react'
-
-// export const dashboard =async () => {
-//   return (
-//     <div>dashboard page in app/ui/dashboard</div>
-//   )
-// }
-
 import Link from 'next/link';
 import React from 'react';
 
-export default function dashboard() {
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+
+// export default function dashboard() {
+//   return (
+//     <div>
+//       dashboard page in app/ui/dashboard
+//       <Link href="/ui/dashboard/invoices">
+//         <button
+//           type="button"
+//           className="flex items-center gap-5 self-start rounded-lg bg-emerald-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-slate-600 md:text-base"
+//         >
+//           invoices
+//         </button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
-    <div>
-      dashboard page in app/ui/dashboard
-      <Link href="/ui/dashboard/invoices">
-        <button
-          type="button"
-          className="flex items-center gap-5 self-start rounded-lg bg-emerald-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-slate-600 md:text-base"
-        >
-          invoices
-        </button>
-      </Link>
-    </div>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
   );
 }
