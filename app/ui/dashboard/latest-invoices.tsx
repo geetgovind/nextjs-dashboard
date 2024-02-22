ced083f897e729aa39c3120a3797df21c42b9b08
geetgovind <geetg.2902@gmail.com>
2024-02-22 14:52:57 +0530

    Changed dashboard bg-color and added radius in invoices

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -13,10 +13,10 @@ export default async function LatestInvoices({
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
+      <div className="flex grow flex-col justify-between rounded-xl bg-emerald-300 p-4">
         {/* NOTE: comment in this code when you get to this point in the course */}
 
-        <div className="bg-white px-6">
+        <div className="rounded-xl bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
