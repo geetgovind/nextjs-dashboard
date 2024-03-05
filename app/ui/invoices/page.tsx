ba950080c852cb6394af377281ba4d245d6569d7
geetgovind <geetg.2902@gmail.com>
2024-03-05 17:02:37 +0530

    Added search front end in invoices

--- app/ui/invoices/page.tsx
+++ app/ui/invoices/page.tsx
@@ -5,7 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { openSans } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
- 
+
 export default async function Page() {
   return (
     <div className="w-full">
@@ -16,7 +16,7 @@ export default async function Page() {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+       {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense> */}
       <div className="mt-5 flex w-full justify-center">
