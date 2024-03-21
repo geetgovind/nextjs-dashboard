ba304480e0d8f1e1c26608eab5040119a0c4f6a7
geetgovind <geetg.2902@gmail.com>
2024-03-21 15:57:36 +0530

    Completed the Edit Invoice with server side rendering and redirection

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,30 +6,54 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        // const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    // Test it out:
-    // console.log(rawFormData);
-    console.log(FormData);
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+  const { customerId, amount, status } = CreateInvoice.parse({
+    // const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // Test it out:
+  // console.log(rawFormData);
+  console.log(FormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 
-    await sql`INSERT INTO invoices (customer_id, amount, status, date)
+  await sql`INSERT INTO invoices (customer_id, amount, status, date)
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-    revalidatePath('/ui/dashboard/invoices');
-    redirect('/ui/dashboard/invoices');
+  revalidatePath('/ui/dashboard/invoices');
+  redirect('/ui/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/ui/dashboard/invoices');
+  redirect('/ui/dashboard/invoices');
 }
